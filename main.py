c7585ac37fefb7a78ff27be4c12fe4e77bb8beca
ACoolUsername9001 <ACoolName9001@gmail.com>
2021-06-14 23:42:59 +0300

    added owner id

--- main.py
+++ main.py
@@ -392,6 +392,6 @@ if __name__ == '__main__':
     intents.bans = False
     intents.members = True
 
-    bot = ACoolBot(intents=intents, max_messages=10000)
+    bot = ACoolBot(intents=intents, max_messages=10000, owner_id=254671305268264960)
     key = json.load(open('DiscordKey.json'))
     bot.run(key["key"])
